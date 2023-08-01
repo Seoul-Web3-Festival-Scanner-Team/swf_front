2afa7302484afc93b90e0dfad711d6648f69d3b4
song127 <songsstudyadress@gmail.com>
2023-08-01 20:25:36 +0900

    feat: some

--- src/networks/ContractConnector.js
+++ src/networks/ContractConnector.js
@@ -4,14 +4,14 @@ import { ethers } from "ethers";
 const contractAddress = "0x256F3B45E810181A2E48772F49e9B04eE741A6F4";
 const provider = new ethers.getDefaultProvider("https://evm-dev-t3.cronos.org");
 class ContractConnector {
-    async getDatas(key, detail) {
+    async getDatas(address, detail) {
         const contract = new ethers.Contract(contractAddress, ABI, provider);
 
         let result = [];
         let i = 0;
         while (true) {
             try {
-                const data = await contract.datas(key, detail, i);
+                const data = await contract.datas(address, detail, i);
                 i++;
                 result.push({
                     rentType: ethers.toNumber(data.rentType),
@@ -25,5 +25,9 @@ class ContractConnector {
         }
 
         return result; // {rentType, rentStart, rentEnd, contractDate}
+    }
+
+    async addData(address, detail, rentType, rentStart, rentEnd, contractDate) {
+
     }
 }
